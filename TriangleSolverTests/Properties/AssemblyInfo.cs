09b755eb3e2aa76da88aa728fce3967052fb0749
SREENADTH KRISHNA <sreenadthkrishna97@gmail.com>
2023-10-01 23:30:27 +0100

    AnalyzeTriangle - Added UnitTest for a valid equilateral triangle

---
+++ TriangleSolverTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TriangleSolverTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TriangleSolverTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("568b4d6e-a75c-48ae-9f8e-4921b255c9e0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TriangleSolverTests/TriangleTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Security.Policy;
+using TriangleSolver;
+
+namespace TriangleSolverTests
+{
+    [TestClass]
+    public class TriangleTests
+    {
+        #region Test for a valid equilateral triangle. 
+
+        [TestMethod]
+        public void AnalyzeTriangle_VerifyingValidEuilateralTriangleTest()
+        {
+            // Arrange
+            var firstSide = 6;
+            var secondSide = 6;
+            var thirdSide = 6;
+
+            // Act
+            var result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            var actual = "Equilateral triangle";
+            Assert.AreEqual(result, actual);
+        }
+
+        #endregion
+    }
+}
